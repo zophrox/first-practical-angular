0f1d74fc20dc151332facc52d2f8acfc2f00c6d8
zophrox <zophrox@gmail.com>
2020-04-22 21:58:32 +0300

    some new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeUk from '@angular/common/locales/uk';
+
+// the second parameter 'fr-FR' is optional
+registerLocaleData(localeUk, 'uk')
+export interface Card {
+  title:string
+  text:string
+}
 
 @Component({
   selector: 'app-root',
@@ -8,7 +17,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   togle = true
-  
+
+  cards: Card[] =[
+    {title:'card 1',text:'This is card number 1'},
+    {title:'this is card 2',text:'This is card number 2'},
+    {title:'last card',text:'This is card number 3'}
+  ]
+
   togleCards(){
     this.togle = !this.togle
   }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,20 +1,26 @@
-import {Component, OnInit} from '@angular/core';
-
+import {Component, OnInit, Input} from '@angular/core';
+import {Card} from '../app.component'
+import { from } from 'rxjs';
 @Component({
     selector:'app-card',
     templateUrl:'./card.component.html',
     styleUrls:['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
+    @Input() card: Card
+    @Input() index: number
     title ="Card Title"
     text = "It is My first interpolation"
+
+    cardDate: Date = new Date()
+
     textColor:string
+
      ngOnInit(){
      
     }
     changeTitle(){
-        this.title = 'Hello Event'
+        this.card.title = 'Hello Event'
     }
  
 }
